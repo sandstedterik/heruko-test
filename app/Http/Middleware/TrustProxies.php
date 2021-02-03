a745f9ff6b652819a9654f063013a9cbd2a18c77
sandstedterik <erik.k.sandstedt@gmail.com>
2021-02-03 13:55:35 +0100

    review app heroku

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '*'; //heroku added = '*'
 
     /**
      * The headers that should be used to detect proxies.
