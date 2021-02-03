a51a7fa9e429d0dfae7dc45eb1cd0fde5609d2ef
sandstedterik <erik.k.sandstedt@gmail.com>
2021-02-03 09:11:54 +0100

    Added a new postcontroller with resource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,37 +3,84 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post;
-use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    //
-    public function index() {
-        $posts = Post::all();
-        return view('posts', compact('posts'));
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
     }
 
-    public function create() {
-        $data = request()->all();
-        $post = Post::create($data);
-
-        return redirect('posts');
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    public function edit(){
-        $post = Post::find($id);
-
-        return view('posts.edit', [
-            'posts' => $posts,
-       ]);
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function destroy($id) {
-        // from net ninja
-        $post = Post::findOrFail($id);
-        $post->delete();
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-        return redirect('/posts');
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
+
+
